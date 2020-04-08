4c2fd26cb76169b78fa889f710fa18deadf04e89
Asei Sugiyama <asei.sugiyama@repro.io>
2020-04-08 20:26:02 +0900

    init

---
+++ test_hiker.py
@@ -0,0 +1,8 @@
+# Usage : $ pytest
+import hiker
+
+
+def test_life_the_universe_and_everything():
+    """a simple example to start you off"""
+    douglas = hiker.Hiker()
+    assert douglas.answer() == 42
