7c76bc18b0f80addb9b427171b63fc13985fa86b
Asei Sugiyama <asei.sugiyama@repro.io>
2020-04-10 19:42:50 +0900

    Add result of tdd hands on.

--- hiker.py
+++ hiker.py
@@ -1,4 +1,17 @@
-class Hiker:
+def fizzbuzz(n):
+    if n % 15 == 0:
+        return "FizzBuzz"
+    if n % 3 == 0:
+        return "Fizz"
+    if n % 5 == 0:
+        return "Buzz"
+    return n
 
-    def answer(self):
-        return 6 * 9
+
+def main():
+    for i in range(1, 101):
+        print(fizzbuzz(i))
+
+
+if __name__ == "__main__":
+    main()
--- test_hiker.py
+++ test_hiker.py
@@ -1,8 +1,45 @@
 # Usage : $ pytest
-import hiker
+from hiker import fizzbuzz
+import subprocess
 
 
-def test_life_the_universe_and_everything():
-    """a simple example to start you off"""
-    douglas = hiker.Hiker()
-    assert douglas.answer() == 42
+def test_fizzbuzz_return_n():
+    assert fizzbuzz(1) == 1
+    assert fizzbuzz(2) == 2
+
+
+def test_fizzbuzz_return_fizz():
+    assert fizzbuzz(3) == "Fizz"
+    assert fizzbuzz(99) == "Fizz"
+
+
+def test_fizzbuzz_return_buzz():
+    assert fizzbuzz(5) == "Buzz"
+    assert fizzbuzz(10) == "Buzz"
+    assert fizzbuzz(100) == "Buzz"
+
+
+def test_fizzbuzz_return_fizzbuzz():
+    assert fizzbuzz(15) == "FizzBuzz"
+    assert fizzbuzz(30) == "FizzBuzz"
+    assert fizzbuzz(90) == "FizzBuzz"
+
+
+def test_fizzbuzz_get_lines():
+    """
+    :param cmd: str 実行するコマンド.
+    :rtype: generator
+    :return: 標準出力 (行毎).
+    """
+    cmd = "python hiker.py"
+    proc = subprocess.Popen(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+    )
+    line = proc.stdout.readline().decode("utf-8")
+    assert line.split("\n")[0] == "1"
+
+    line = proc.stdout.readline().decode("utf-8")
+    assert line.split("\n")[0] == "2"
+
+    line = proc.stdout.readline().decode("utf-8")
+    assert line.split("\n")[0] == "Fizz"
